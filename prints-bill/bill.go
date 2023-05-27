57bf39eea24568d8ed474ea963e7050d34134569
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-05-27 17:29:19 +0700

    interface

--- prints-bill/bill.go
+++ prints-bill/bill.go
@@ -22,11 +22,11 @@ type Invoice struct {
 	Performances []Performance `json:"performances"`
 }
 
-func playKind(play Play) string {
+func (play Play) playKind() string {
 	return play.Kind
 }
 
-func playName(play Play) string {
+func (play Play) playName() string {
 	return play.Name
 }
 
@@ -36,7 +36,7 @@ func playFor(plays Plays, perf Performance) Play {
 
 func (play Play) amountFor(audience int) float64 {
 	amount := 0.0
-	switch play.Kind {
+	switch play.playKind() {
 	case "tragedy":
 		amount = 40000
 		if audience > 30 {
@@ -49,7 +49,7 @@ func (play Play) amountFor(audience int) float64 {
 		}
 		amount += 300 * float64(audience)
 	default:
-		panic(fmt.Sprintf("unknow type: %s", play.Kind))
+		panic(fmt.Sprintf("unknow type: %s", play.playKind()))
 	}
 	return amount
 }
@@ -57,7 +57,7 @@ func (play Play) amountFor(audience int) float64 {
 func (play Play) volumeCreditsFor(audience int) float64 {
 	credits := 0.0
 	credits += math.Max(float64(audience-30), 0)
-	if play.Kind == "comedy" {
+	if play.playKind() == "comedy" {
 		credits += math.Floor(float64(audience / 5))
 	}
 	return credits
@@ -81,8 +81,15 @@ func (rates Rates) totalVolumeCredits() float64 {
 	return credits
 }
 
+type Player interface {
+	playKind() string
+	playName() string
+	amountFor(audience int) float64
+	volumeCreditsFor(audience int) float64
+}
+
 type Rate struct {
-	Play     Play
+	Play     Player
 	Amount   float64
 	Audience int
 	Credits  float64
@@ -98,7 +105,7 @@ type Bill struct {
 func renderPlainText(bill Bill) string {
 	result := fmt.Sprintf("Statement for %s\n", bill.Customer)
 	for _, rate := range bill.Rates {
-		result += fmt.Sprintf("  %s: $%.2f (%d seats)\n", rate.Play.Name, rate.Amount/100, rate.Audience)
+		result += fmt.Sprintf("  %s: $%.2f (%d seats)\n", rate.Play.playName(), rate.Amount/100, rate.Audience)
 	}
 	result += fmt.Sprintf("Amount owed is $%.2f\n", bill.TotalAmount/100)
 	result += fmt.Sprintf("you earned %.0f credits\n", bill.TotalVolumeCredits)
@@ -110,7 +117,7 @@ func renderPlainText(bill Bill) string {
 // 	result += "<table>\n"
 // 	result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>"
 // 	for _, rate := range bill.Rates {
-// 		result += fmt.Sprintf("<tr><td>%s</td><td>%d</td><td>$%.2f</td></tr>\n", rate.Play.Name, rate.Audience, rate.Amount/100)
+// 		result += fmt.Sprintf("<tr><td>%s</td><td>%d</td><td>$%.2f</td></tr>\n", rate.Play.playName(), rate.Audience, rate.Amount/100)
 // 	}
 // 	result += "</table>\n"
 // 	result += fmt.Sprintf("<p>Amount owed is <em>$%.2f</em></p>\n", bill.TotalAmount/100)
