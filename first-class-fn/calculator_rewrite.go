cc711788fedeeebf826131e6728fe4b7eda3c235
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-06-03 15:49:34 +0700

    hof

--- first-class-fn/calculator_rewrite.go
+++ first-class-fn/calculator_rewrite.go
@@ -3,40 +3,62 @@ package main
 import (
 	"fmt"
 	"math"
+	"net/http"
 )
 
 type Calculator struct {
 	acc float64
 }
 
-type opfunc func(float64, float64) float64
+type opfunc func(float64) float64
 
-func (c *Calculator) Do(op opfunc, v float64) float64 {
-	c.acc = op(c.acc, v)
+func (c *Calculator) Do(fn opfunc) float64 {
+	c.acc = fn(c.acc)
 	return c.acc
 }
 
-func Add(a, b float64) float64 { return a + b }
+func Add(a float64) func(float64) float64 {
+	return func(acc float64) float64 {
+		return acc + a
+	}
+}
 
-func Sub(a, b float64) float64 { return a - b }
-func Mul(a, b float64) float64 { return a * b }
+func Sub(a float64) func(float64) float64 {
+	return func(acc float64) float64 {
+		return acc - a
+	}
+}
+
+func Mul(a float64) func(float64) float64 {
+	return func(acc float64) float64 {
+		return acc * a
+	}
+}
+
+func Sqrt() func(float64) float64 {
+	return func(acc float64) float64 {
+		return math.Sqrt(acc)
+	}
+}
 
 func main() {
-	var c Calculator
-	fmt.Println(c.Do(Add, 5)) // 5
-	fmt.Println(c.Do(Sub, 3)) // 2
-	fmt.Println(c.Do(Mul, 8)) // 16
-	c.Do(Sqrt, 0)             // operand ignored
-
 	// TO BE:
-	// var c Calculator
-	// c.Do(Add(5))
-	// c.Do(Sub(3))
-	// c.Do(Mul(8))
-	// c.Do(Sqrt())
-	// c.Do(math.Sqrt) // 1.41421356237 c.Do(math.Cos) // 0.99969539804
+	var c Calculator
+	c.Do(Add(8))
+	c.Do(Sub(4))
+	c.Do(Mul(4))
+	c.Do(Sqrt())
+	c.Do(math.Sqrt)
+	// c.Do(math.Cos)
+
+	fmt.Println("Accumulator:", c.acc)
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+	}
+
+	http.HandleFunc("/", handler)
 }
 
-func Sqrt(n, _ float64) float64 {
-	return math.Sqrt(n)
-}
+// func Sqrt(n, _ float64) float64 {
+// 	return math.Sqrt(n)
+// }
---
+++ first-class-fn/hof.go
@@ -0,0 +1,3 @@
+package main
+
+// Higher-order function
