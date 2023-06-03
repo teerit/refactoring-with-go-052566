358875591958851d77ec298ea2c7336558e72de5
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-06-03 15:05:33 +0700

    first class function

--- first-class-fn/calculator_rewrite.go
+++ first-class-fn/calculator_rewrite.go
@@ -23,12 +23,18 @@ func Mul(a, b float64) float64 { return a * b }
 
 func main() {
 	var c Calculator
-	fmt.Println(c.Do(Add, 5))       // 5
-	fmt.Println(c.Do(Sub, 3))       // 2
-	fmt.Println(c.Do(Mul, 8))       // 16
-	c.Do(Sqrt, 0) // operand ignored
-}
+	fmt.Println(c.Do(Add, 5)) // 5
+	fmt.Println(c.Do(Sub, 3)) // 2
+	fmt.Println(c.Do(Mul, 8)) // 16
+	c.Do(Sqrt, 0)             // operand ignored
 
+	// TO BE:
+	// var c Calculator
+	// c.Do(Add(1))
+	// c.Do(Add(1))
+	// c.Do(Sqrt())
+	// c.Do(math.Sqrt) // 1.41421356237 c.Do(math.Cos) // 0.99969539804
+}
 
 func Sqrt(n, _ float64) float64 {
 	return math.Sqrt(n)
